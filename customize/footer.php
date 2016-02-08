9061e5fc5a8c537b2011608ea6170e131c710a40
Ioannis Kokkinis <info@upg.gr>
2016-02-08 20:44:57 +0000

    Delete footer.php

--- customize/footer.php
+++
@@ -1,1 +0,0 @@
-Created by <a href="http://www.x-mad.com">x-mad.com</a> | You want the <a href="https://github.com/upggr/x-mad" target="new">code?</a> | <a href="mailto:info@x-mad.com">email me?</a>
