4fad2988d2c361136eda8d6617e05230390ad84d
Ioannis Kokkinis <info@upg.gr>
2016-02-08 20:45:23 +0000

    Delete socialbuttonstop.php

--- customize/socialbuttonstop.php
+++
@@ -1,4 +0,0 @@
-  <a href="https://www.facebook.com/pages/x-madcom/137987969596393"><img src="images/facebook.png" width="26" height="26" alt="facebook" /> </a>
-  <a href="#2"><img src="images/twitter.png" width="26" height="26" alt="twitter" />  </a>
-  <a href="#3"><img src="images/linkedin.png" width="26" height="26" alt="linkedin" />   </a>
-  <a href="mailto:info@x-mad.com"><img src="images/email.png" width="26" height="26" alt="email" /></a>
